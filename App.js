25a3dcd9706934181cd7a7b96734728d8006e74a
David Omondi Nyalwal <davidomondi842@gmail.com>
2024-03-17 23:45:59 +0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View ,Pressable} from 'react-native';
+import Icon from "react-native-ico-material-design";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+var iconHeight=26;
+var iconWidth=26;
+
+export default class App extends React.Component{
+  state={
+    screenText:'Press a button!'
+  }
+
+  changeText=(text)=>{
+    console.log(text + 'has been pressed!')
+    this.setState({
+      screenText:text
+    })
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+      <View>
+        <Text style={{fontsize:30, color:'white'}}>{this.state.screenText}</Text>
+        <StatusBar style="light" />
+        </View>
+
+        <View style={styles.NavContainer}>
+          <View style={styles.Navbar}>
+            <Pressable onPress={()=> this.changeText('Favourites')} style={styles.IconBehave}>
+
+            </Pressable>
+          </View>
+        </View>
+      </View>
+    );
+  }
 }
+  
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#3962ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  NavContainer:{
+    position: 'absolute',
+    alignItems:'items',
+    bottom:20,
+  },
+  Navbar:{
+    flexDirection:'row',
+    backgroundColor:'#eee',
+    width:'90%',
+    justifyContent:'space evenly',
+    borderRadius:40
+  },
+  IconBehave:{
+    padding:14
+  }
 });
